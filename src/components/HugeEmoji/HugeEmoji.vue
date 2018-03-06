61f9ddefcca13df580d270ad9933299a04be300a
Brad Adams <hi@breadadams.com>
2018-03-06 01:34:39 +0100

    Create `HugeEmoji` component

---
+++ src/components/HugeEmoji/HugeEmoji.vue
@@ -0,0 +1,22 @@
+<template>
+  <span class="huge-emoji"><slot></slot></span>
+</template>
+
+<script>
+export default {
+  name: 'HugeEmoji'
+}
+</script>
+
+<style lang="scss" scoped>
+.huge-emoji {
+  font-size: 100px;
+  position: absolute;
+  left: 0;
+  top: 50%;
+  width: 100%;
+  text-align: center;
+  transform: translateY(-50%);
+  margin: 0;
+}
+</style>
